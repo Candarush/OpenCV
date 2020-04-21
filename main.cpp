d176fb383d946050a1bb940433a24eae995b5dfe
Vladislav Akimov <candarush@gmail.com>
2020-04-21 13:35:12 +0300

    2

--- main.cpp
+++ main.cpp
@@ -249,14 +249,12 @@ void OpticalFlow(string imagePath, int speed)
         cvtColor(frame2, next, COLOR_BGR2GRAY);
         Mat flow(prvs.size(), CV_32FC2);
         calcOpticalFlowFarneback(prvs, next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
-        // visualization
         Mat flow_parts[2];
         split(flow, flow_parts);
         Mat magnitude, angle, magn_norm;
         cartToPolar(flow_parts[0], flow_parts[1], magnitude, angle, true);
         normalize(magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
         angle *= ((1.f / 360.f) * (180.f / 255.f));
-        //build hsv image
         Mat _hsv[3], hsv, hsv8, bgr;
         _hsv[0] = angle;
         _hsv[1] = Mat::ones(angle.size(), CV_32F);
